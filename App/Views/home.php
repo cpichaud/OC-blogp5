fb9ec75c02e63ce40ab746eed996afe131d6ede1
cpichaud <camillepichaud44@gmail.com>
2022-11-10 15:35:25 +0100

    feat: add front in home page

--- App/Views/home.php
+++ App/Views/home.php
@@ -1,9 +1,85 @@
 <!--HEADER-->
 <?php require "header.php"?>
+<section class="top-home">
+    <div class="container">
+        <div class="logo text-center text-light">
+            <!--<img src="../../public/images/home/logo.jpg" alt="logo" style="width: 300px;">-->
+            <div class="name-home">    
+                <h1 class="border-top border-bottom">CAMILLE PICHAUD</h1>
+                <h2>Développeur d'application</h2>
+            </div>
+        </div>
+    </div>
+</section>
 <main class="container">
+    <section>
+        
+    </section>
+    <section class="form-home">
+    <form>
+  <!-- 2 column grid layout with text inputs for the first and last names -->
+  <div class="row mb-4">
+    <div class="col">
+      <div class="form-outline">
+        <input type="text" id="form3Example1" class="form-control" />
+        <label class="form-label" for="form3Example1">First name</label>
+      </div>
+    </div>
+    <div class="col">
+      <div class="form-outline">
+        <input type="text" id="form3Example2" class="form-control" />
+        <label class="form-label" for="form3Example2">Last name</label>
+      </div>
+    </div>
+  </div>
+
+  <!-- Email input -->
+  <div class="form-outline mb-4">
+    <input type="email" id="form3Example3" class="form-control" />
+    <label class="form-label" for="form3Example3">Email address</label>
+  </div>
+
+  <!-- Password input -->
+  <div class="form-outline mb-4">
+    <input type="password" id="form3Example4" class="form-control" />
+    <label class="form-label" for="form3Example4">Password</label>
+  </div>
+
+  <!-- Checkbox -->
+  <div class="form-check d-flex justify-content-center mb-4">
+    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
+    <label class="form-check-label" for="form2Example33">
+      Subscribe to our newsletter
+    </label>
+  </div>
+
+  <!-- Submit button -->
+  <button type="submit" class="btn btn-primary btn-block mb-4">Sign up</button>
+
+  <!-- Register buttons -->
+  <div class="text-center">
+    <p>or sign up with:</p>
+    <button type="button" class="btn btn-primary btn-floating mx-1">
+      <i class="fab fa-facebook-f"></i>
+    </button>
+
+    <button type="button" class="btn btn-primary btn-floating mx-1">
+      <i class="fab fa-google"></i>
+    </button>
+
+    <button type="button" class="btn btn-primary btn-floating mx-1">
+      <i class="fab fa-twitter"></i>
+    </button>
+
+    <button type="button" class="btn btn-primary btn-floating mx-1">
+      <i class="fab fa-github"></i>
+    </button>
+  </div>
+</form>
+    </section>
 
 <?= $content; ?>
-
+  
 </main>
 
 <?php require "footer.php"?>
