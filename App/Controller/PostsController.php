4c122b15b526b76cce550aa8ad713d8aaa0942a8
cpichaud <camillepichaud44@gmail.com>
2022-11-18 14:47:26 +0100

    feat: add manager for post and comment

--- App/Controller/PostsController.php
+++ App/Controller/PostsController.php
@@ -2,16 +2,39 @@
 
 namespace App\Controller;
 
+use App\Model\PostManager;
 use App\Controller\Controller as Controller ;
 
 class PostsController extends Controller{
 
+
+     /**
+     * @var PostManager
+     */
+    private $postManager;
+
+    public function __construct()
+    {
+        $this->postManager = new PostManager();
+    }
+
+
     public function show(){
-        
+ 
+     
+        $postTest = $this->postManager->findAll();
+
+        foreach($postTest as $post){
+            var_dump($post);
+        }
         $content = "ceci est le contenu de mon post.";
 
-        $arrayToTemplate = ['title' => 'Mon post', 'content' => $content ,'posts' =>[]];
-
+        $arrayToTemplate = [
+            'title' => 'Camille PICHAUD', 
+            'Accueil' => [],
+            'post' => $post,
+        ];
+        
         $this->render($arrayToTemplate, __CLASS__);
         
     }
